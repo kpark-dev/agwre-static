70f6c74f80d2d3be0b1ddf211c30101256e7c301
gmdgville <glorianne@annix.com.au>
2024-08-15 18:50:07 +0800

    saved homes tablet

--- web/static/saved-homes-2.php
+++ web/static/saved-homes-2.php
@@ -14,8 +14,89 @@ require('includes/header.php');
               <p class="mb-0 text-xxs md:text-[11px] lg:text-xs">Showıng all 8 homes</p>
             </div>
           </div>
-          <!-- filter -->
-          <div class="max-w-[1200px] w-full mx-auto flex justify-between flex-col md:flex-row gap-6 items-start md:items-center mb-6 md:mb-[34px]">
+          <!-- tablet filter -->
+          <div class=" hidden md:block lg:hidden">
+            <div class="max-w-[1200px] w-full mx-auto shadow-[0px_3px_18px_rgba(0,0,0,0.5_)] bg-primary mt-[47px] rounded-[10px] flex items-center justify-between gap-6 py-4 pl-[23px] pr-[18px] mb-[37px]">
+              <input type="text" class="w-full h-[31px] max-w-[188px] border-[#707070] bg-white rounded-[5px] border border-solid !ring-0 !outline-0" />
+              <div class="flex flex-col">
+                <p class="mb-0 text-lightWhite text-xs">Status</p>
+                <details class="dropdown">
+                  <summary class="text-lightWhite text-xs flex gap-2 items-center"><span class="">FOR Sale</span></summary>
+                  <ul class="menu dropdown-content bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
+                    <li><a>Item</a></li>
+                    <li><a>Item</a></li>
+                  </ul>
+                </details>
+              </div>
+              <div class="flex flex-col">
+                <p class="mb-0 text-lightWhite text-xs">Type</p>
+                <details class="dropdown">
+                  <summary class="text-lightWhite text-xs flex gap-2 items-center"><span class="">Condos</span><i class="bg-icon-chevron w-[6px] h-1 no-repeat bg-center">&nbsp;</i></summary>
+                  <ul class="menu dropdown-content bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
+                    <li><a>Item</a></li>
+                    <li><a>Item</a></li>
+                  </ul>
+                </details>
+              </div>
+              <div class="flex flex-col min-w-[47px]">
+                <p class="mb-0 text-lightWhite text-xs">Price</p>
+                <details class="dropdown">
+                  <summary class="text-lightWhite text-xs flex gap-2 items-center"><span class="">1m-2m</span><i class="bg-icon-chevron w-[6px] h-1 no-repeat bg-center">&nbsp;</i></summary>
+                  <ul class="menu dropdown-content bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
+                    <li><a>Item</a></li>
+                    <li><a>Item</a></li>
+                  </ul>
+                </details>
+              </div>
+              <div class="flex flex-col min-w-[74px]">
+                <p class="mb-0 text-lightWhite text-xs">Beds & Baths</p>
+                <details class="dropdown">
+                  <summary class="text-lightWhite text-xs flex gap-2 items-center"><span class="">3 - 2 </span></summary>
+                  <ul class="menu dropdown-content bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
+                    <li><a>Item</a></li>
+                    <li><a>Item</a></li>
+                  </ul>
+                </details>
+              </div>
+              <div class="flex flex-col">
+                <p class="mb-0 text-lightWhite text-xs">Age</p>
+                <details class="dropdown">
+                  <summary class="text-lightWhite text-xs flex gap-2 items-center"><span class="">+10 </span></summary>
+                  <ul class="menu dropdown-content bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
+                    <li><a>Item</a></li>
+                    <li><a>Item</a></li>
+                  </ul>
+                </details>
+              </div>
+              <button class="rounded-[10px] bg-white flex items-center justify-center w-[31px] h-[31px] min-w-[31px] min-h-[31px]"><img src="images/search.svg" /></button>
+            </div>
+            <div class="w-full flex justify-between items-center my-[37px]">
+              <div class="flex items-center gap-5">
+                <a href="#" class="btnSolid w-[113px] lg:w-[141px] !bg-[#b9c5d1] !border-[#b9c5d1] hover:!border-primary text-white hover:!text-white hover:!bg-primary !h-[38px]">Compare</a>
+                <a href="#" class="btnSolid w-[113px] lg:w-[141px] !h-[38px]">Map View</a>
+              </div>
+              <div class="flex flex-col gap-3 items-end">
+                <p class="text-xxs tracking-[3px] font-semibold mb-0">North Las Vegas (8 results)</p>
+                <div class="flex gap-[30px] items-center">
+                  <a href="#" class="tracking-[4px] text-xxs">All filters</a>
+                  <a href="#" class="tracking-[4px] text-xxs">Save search </a>
+                  <details class="dropdown">
+                    <summary class="flex gap-2 items-center">
+                      <p class="text-primary mb-0 text-xxs md:text-xs">Sort by </p><i class="bg-icon-chevron-primary w-[6px] h-1 no-repeat bg-center">&nbsp;</i>
+                    </summary>
+                    <ul class="menu dropdown-content bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
+                      <li><a>Option</a></li>
+                      <li><a>Option</a></li>
+                      <li><a>Option</a></li>
+                      <li><a>Option</a></li>
+                    </ul>
+                  </details>
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- desktop and mobile filter -->
+          <div class="max-w-[1200px] w-full mx-auto flex md:hidden lg:flex justify-between flex-col md:flex-row gap-6 items-start md:items-center mb-6 md:mb-[34px]">
             <div class="w-full md:w-auto overflow-y-auto">
               <div class="flex items-start gap-[26px] lg:gap-10 h-[41px] border border-solid border-[#0e202c] rounded-[5px] overflow-y-auto md:h-auto md:border-0 px-3.5 py-[13px] md:px-0 md:py-0">
                 <p class="text-xxs lg:text-xs mb-0 tracking-[2px]">Type</p>
