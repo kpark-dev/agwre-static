fda696eedc43a4c8481c32c82844df7ec974ab97
gmdgville <glorianne@annix.com.au>
2024-08-16 22:53:30 +0800

    compare page 3

---
+++ web/static/compare-3.php
@@ -0,0 +1,118 @@
+<?php
+require('includes/header.php');
+?>
+
+<div class="main-holder">
+  <main class="main-content">
+    <div class="w-full min-h-[calc(100vh_-_59px)] md:min-h-[calc(100vh_-_104px)] xxl:min-h-[100vh] flex justify-start items-start">
+      <div class="row !mt-0 md:!px-10 !px-0 overflow-y-auto">
+        <div class="fullwidth-col  pb-20 ">
+          <!-- compare -->
+          <div class="bg-primary md:mg-transparent max-w-[1200px] md:w-full md:mx-auto mb-[49px] md:mb-[62px] lg:mb-[74px] mt-[30px] md:mt-0">
+            <div class="max-w-[1200px] mx-auto shadow-[0px_3px_18px_rgba(0,0,0,0.5_)] bg-primary rounded-[10px] flex items-center justify-between gap-6  px-5 md:px-[31px] lg:px-[57px]">
+              <div class="h-full flex flex-col justify-between items-start min-h-[105px] md:min-h-[135px] lg:min-h-[215px] min-w-[101px] md:min-w-[122px] lf:min-w-[167px] bg-primary">
+                <a href="#" class="flex gap-4 items-center">
+                  <img src="images/back.svg" alt="" />
+                  <p class="mb-0 text-white text-xxs">Back to lıstıngs </p>
+                </a>
+                <div class="flex gap-2 flex-col-reverse md:flex-col">
+                  <p class="mb-0 text-[15px] md:text-[17px] lg:text-lg text-white">Attrıbutes</p>
+                  <a href="#" class="text-white text-[8px] md:text-xxs lg:text-xs">Edit <span class="hidden md:inline-block">attrıbutes</span></a>
+                </div>
+              </div>
+              <div class="flex items-start gap-2.5 md:gap-[29px] xxl:gap-[62px] py-[18px] md:py-8 lg:py-[50px] bg-primary">
+                <div class="flex flex-col items-start gap-[7px] md:gap-[9px] lg:gap-4">
+                  <img src="images/blogarticle-2.png" alt="" loading="lazy" class="object-cover rounded-[10px] w-full h-full min-w-[97px] min-h-[83px] max-w-[97px] max-h-[83px] md:min-w-[132px] md:min-h-[87px] md:max-w-[132px] md:max-h-[87px] lg:min-w-[224px] lg:min-h-[147px] lg:max-w-[224px] lg:max-h-[147px]" />
+                  <p class="mb-0 text-[11px] md:text-[15px] lg:text-[26px] font-cirka text-white max-w-[97px] md:max-w-[132px] lg:max-w-[224px]">331 Cactus Sands Ave North LV</p>
+                </div>
+                <div class="flex flex-col items-start gap-[7px] md:gap-[9px] lg:gap-4">
+                  <img src="images/blogarticle-2.png" alt="" loading="lazy" class="object-cover rounded-[10px] w-full h-full min-w-[97px] min-h-[83px] max-w-[97px] max-h-[83px] md:min-w-[132px] md:min-h-[87px] md:max-w-[132px] md:max-h-[87px] lg:min-w-[224px] lg:min-h-[147px] lg:max-w-[224px] lg:max-h-[147px]" />
+                  <p class="mb-0 text-[11px] md:text-[15px] lg:text-[26px] font-cirka text-white max-w-[97px] md:max-w-[132px] lg:max-w-[224px]">331 Cactus Sands Ave North LV</p>
+                </div>
+                <div class="flex flex-col items-start gap-[7px] md:gap-[9px] lg:gap-4">
+                  <img src="images/blogarticle-2.png" alt="" loading="lazy" class="object-cover rounded-[10px] w-full h-full min-w-[97px] min-h-[83px] max-w-[97px] max-h-[83px] md:min-w-[132px] md:min-h-[87px] md:max-w-[132px] md:max-h-[87px] lg:min-w-[224px] lg:min-h-[147px] lg:max-w-[224px] lg:max-h-[147px]" />
+                  <p class="mb-0 text-[11px] md:text-[15px] lg:text-[26px] font-cirka text-white max-w-[97px] md:max-w-[132px] lg:max-w-[224px]">331 Cactus Sands Ave North LV</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- details -->
+          <div class="max-w-[1200px] md:w-full md:mx-auto mt-[30px] md:mt-0">
+            <div class="max-w-[1200px] mx-auto flex items-start justify-between gap-6 py-0 px-5 md:px-[31px] lg:px-[57px]">
+              <div class="h-full flex flex-col justify-between items-start min-w-[101px] md:min-w-[122px] lf:min-w-[167px] gap-[22px] md:gap-[30px] lg:gap-[46px]">
+                <p class="font-semibold mb-0 text-[8px] md:text-xxs lg:text-s">Neıghborhoods</p>
+                <p class="font-semibold mb-0 text-[8px] md:text-xxs lg:text-s">Lıstıng prıce </p>
+                <p class="font-semibold mb-0 text-[8px] md:text-xxs lg:text-s">Lıstıng date </p>
+                <p class="font-semibold mb-0 text-[8px] md:text-xxs lg:text-s">sqft </p>
+                <p class="font-semibold mb-0 text-[8px] md:text-xxs lg:text-s">$/Sqft </p>
+                <p class="font-semibold mb-0 text-[8px] md:text-xxs lg:text-s">Beds </p>
+                <p class="font-semibold mb-0 text-[8px] md:text-xxs lg:text-s">Baths </p>
+                <p class="font-semibold mb-0 text-[8px] md:text-xxs lg:text-s">Year built </p>
+                <p class="font-semibold mb-0 text-[8px] md:text-xxs lg:text-s">Strata fees </p>
+                <p class="font-semibold mb-0 text-[8px] md:text-xxs lg:text-s"># of Parking </p>
+                <p class="font-semibold mb-0 text-[8px] md:text-xxs lg:text-s">Property tax </p>
+                <p class="font-semibold mb-0 text-[8px] md:text-xxs lg:text-s">Sale prıce </p>
+                <p class="font-semibold mb-0 text-[8px] md:text-xxs lg:text-s">% Market Change </p>
+                <p class="font-semibold mb-0 text-[8px] md:text-xxs lg:text-s">Sale date </p>
+              </div>
+              <div class="flex items-start gap-2.5 md:gap-[29px] xxl:gap-[62px]">
+                <div class="flex flex-col items-start gap-[22px] md:gap-[30px] lg:gap-[46px] w-full h-full min-w-[97px] max-w-[97px] md:min-w-[132px] md:max-w-[132px] lg:min-w-[224px] lg:max-w-[224px]">
+                  <p class="mb-0 text-[8px] md:text-xxs lg:text-s">North Carolina </p>
+                  <p class="mb-0 text-[8px] md:text-xxs lg:text-s">$1,900,000 </p>
+                  <p class="mb-0 text-[8px] md:text-xxs lg:text-s">16/08/2013 </p>
+                  <p class="mb-0 text-[8px] md:text-xxs lg:text-s">2,416 </p>
+                  <p class="mb-0 text-[8px] md:text-xxs lg:text-s">$/Sqft </p>
+                  <p class="mb-0 text-[8px] md:text-xxs lg:text-s">4</p>
+                  <p class="mb-0 text-[8px] md:text-xxs lg:text-s">3</p>
+                  <p class="mb-0 text-[8px] md:text-xxs lg:text-s">1991 </p>
+                  <p class="mb-0 text-[8px] md:text-xxs lg:text-s">$17.84 </p>
+                  <p class="mb-0 text-[8px] md:text-xxs lg:text-s">2</p>
+                  <p class="mb-0 text-[8px] md:text-xxs lg:text-s">$ 7,000.00 </p>
+                  <p class="mb-0 text-[8px] md:text-xxs lg:text-s">$ 7,000.00 </p>
+                  <p class="mb-0 text-[8px] md:text-xxs lg:text-s">8.5% </p>
+                  <p class="mb-0 text-[8px] md:text-xxs lg:text-s">5/19/12 </p>
+                </div>
+                <div class="flex flex-col items-start gap-[22px] md:gap-[30px] lg:gap-[46px] w-full h-full min-w-[97px] max-w-[97px] md:min-w-[132px] md:max-w-[132px] lg:min-w-[224px] lg:max-w-[224px]">
+                  <p class="mb-0 text-[8px] md:text-xxs lg:text-s">North Carolina </p>
+                  <p class="mb-0 text-[8px] md:text-xxs lg:text-s">$1,900,000 </p>
+                  <p class="mb-0 text-[8px] md:text-xxs lg:text-s">16/08/2013 </p>
+                  <p class="mb-0 text-[8px] md:text-xxs lg:text-s">2,416 </p>
+                  <p class="mb-0 text-[8px] md:text-xxs lg:text-s">$/Sqft </p>
+                  <p class="mb-0 text-[8px] md:text-xxs lg:text-s">4</p>
+                  <p class="mb-0 text-[8px] md:text-xxs lg:text-s">3</p>
+                  <p class="mb-0 text-[8px] md:text-xxs lg:text-s">1991 </p>
+                  <p class="mb-0 text-[8px] md:text-xxs lg:text-s">$17.84 </p>
+                  <p class="mb-0 text-[8px] md:text-xxs lg:text-s">2</p>
+                  <p class="mb-0 text-[8px] md:text-xxs lg:text-s">$ 7,000.00 </p>
+                  <p class="mb-0 text-[8px] md:text-xxs lg:text-s">$ 7,000.00 </p>
+                  <p class="mb-0 text-[8px] md:text-xxs lg:text-s">8.5% </p>
+                  <p class="mb-0 text-[8px] md:text-xxs lg:text-s">5/19/12 </p>
+                </div>
+                <div class="flex flex-col items-start gap-[22px] md:gap-[30px] lg:gap-[46px] w-full h-full min-w-[97px] max-w-[97px] md:min-w-[132px] md:max-w-[132px] lg:min-w-[224px] lg:max-w-[224px]">
+                  <p class="mb-0 text-[8px] md:text-xxs lg:text-s">North Carolina </p>
+                  <p class="mb-0 text-[8px] md:text-xxs lg:text-s">$1,900,000 </p>
+                  <p class="mb-0 text-[8px] md:text-xxs lg:text-s">16/08/2013 </p>
+                  <p class="mb-0 text-[8px] md:text-xxs lg:text-s">2,416 </p>
+                  <p class="mb-0 text-[8px] md:text-xxs lg:text-s">$/Sqft </p>
+                  <p class="mb-0 text-[8px] md:text-xxs lg:text-s">4</p>
+                  <p class="mb-0 text-[8px] md:text-xxs lg:text-s">3</p>
+                  <p class="mb-0 text-[8px] md:text-xxs lg:text-s">1991 </p>
+                  <p class="mb-0 text-[8px] md:text-xxs lg:text-s">$17.84 </p>
+                  <p class="mb-0 text-[8px] md:text-xxs lg:text-s">2</p>
+                  <p class="mb-0 text-[8px] md:text-xxs lg:text-s">$ 7,000.00 </p>
+                  <p class="mb-0 text-[8px] md:text-xxs lg:text-s">$ 7,000.00 </p>
+                  <p class="mb-0 text-[8px] md:text-xxs lg:text-s">8.5% </p>
+                  <p class="mb-0 text-[8px] md:text-xxs lg:text-s">5/19/12 </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+  </main>
+</div>
+
+
+<?php
+// require('includes/footer.php');
+?>
